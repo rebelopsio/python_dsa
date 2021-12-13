d36d2dfecc468f5287001c837caa948f14760961
Stephen Morgan <stephen.morgan@machinify.com>
2021-12-13 14:39:23 -0800

    added Stack

---
+++ Stack/Stack.py
@@ -0,0 +1,24 @@
+"""
+Stack data structure
+"""
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def get_items(self):
+        return self.items
