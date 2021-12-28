b41a254eebe4482bd284281aebed01f2c0515404
Stephen Morgan <stephen.morgan@machinify.com>
2021-12-28 11:00:24 -0800

    added merge function

--- SinglyLinkedLists/SLL.py
+++ SinglyLinkedLists/SLL.py
@@ -157,15 +157,56 @@ class LinkedList:
 
         self.head = _reverse_recursive(cur=self.head, prev=None)
 
+    def merge_sorted(self, llist):
+        p = self.head
+        q = llist.head
+        s = None
+
+        if not p:
+            return q
+        if not q:
+            return p
+
+        if p and q:
+            if p.data <= q.data:
+                s = p
+                p = s.next
+            else:
+                s = q
+                q = s.next
+            new_head = s
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+        if not p:
+            s.next = q
+        if not q:
+            s.next = p
+
+        self.head = new_head
+        return self.head
 
 
+llist_1 = LinkedList()
+llist_2 = LinkedList()
 
-llist = LinkedList()
-llist.append("A")
-llist.append("B")
-llist.append("C")
-llist.append("D")
+llist_1.append(1)
+llist_1.append(5)
+llist_1.append(7)
+llist_1.append(9)
+llist_1.append(10)
 
-llist.reverse_recursive()
+llist_2.append(2)
+llist_2.append(3)
+llist_2.append(4)
+llist_2.append(6)
+llist_2.append(8)
 
-llist.print_list()
+llist_1.merge_sorted(llist_2)
+llist_1.print_list()
